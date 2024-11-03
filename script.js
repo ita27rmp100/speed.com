011ca33f04942b65bbe29729de9a3055bcba9637
Tayeb Abderahim Ismail <ita27rmp100@gmail.com>
2024-11-03 18:36:57 +0100

    finishing the front-end of internet speed card

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// changing theme
 let color = [
     '#ff040491',
     '#0056b3',
@@ -15,4 +16,28 @@ $('#change').click(function() {
     $('#change').css('background',color[indexColor])
     indexColor++
     if(indexColor==color.length){indexColor=0}
+})
+// display the history of this device
+let history = ''
+for (let i = 0; i < localStorage.length; i++) {
+    key = localStorage.key(i)
+    history += `<tr>
+                    <td>${key}</td>
+                    <td>${localStorage.getItem(key)}</td>
+                </tr>`
+}
+$("#history").html(history)
+// test again
+$("#again").click(function(){
+    $("#again").css('background',color[indexColor-1])
+},()=>{$("#again").css('background','white')})
+// exit the notification of subscribition
+$("#exit").click(function() {
+    $("footer").fadeOut()
+    $("body").css({
+        "height": "100vh",
+        "align-items": "center",
+        "position": "static",
+        "padding-bottom":"5px"
+    });
 })
