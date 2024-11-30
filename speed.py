52cdb306fef5b0b6d2de9e362fd39f7a1385e451
Abderahim <ita27rmp100@gmail.com>
2024-11-30 12:24:58 +0100

    deploy the project

--- speed.py
+++ speed.py
@@ -2,10 +2,7 @@ import speedtest
 try :
     l = speedtest.Speedtest()
     download = f'{l.download()/1048576:03.2f} Mbps'
-    print("download : ",download)
     upload = f'{l.upload()/1048576:03.2f} Mbps'
-    print("upload : ",upload)
     ping = f'{l.results.ping:03.2f} Mbps'
-    print("ping : ",ping)
 except speedtest.ConfigRetrievalError :
-    print("Internet speed","there is  no Internet")
+    print("there is  no Internet")
